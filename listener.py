24fb0a7f0e26ff9dc098be4971f784ed37df6083
James MacFarlane <james@macfarlane.com>
2018-03-09 12:19:29 +0100

    restructure listener code, add meme func

--- listener.py
+++ listener.py
@@ -2,6 +2,7 @@
 
 import os
 import time
+from random import randint
 import re
 from slackclient import SlackClient
 from network import check_connection
@@ -11,81 +12,98 @@ sc = SlackClient(os.environ['SLACK_TOKEN'])
 listen_channel = os.environ['SLACK_CHANNEL']
 node_ip = os.environ['K8S_NODE_IP']
 
+usage = """
+"""
+
+timestamp = lambda : time.strftime("%Y-%m-%dT%H:%M:%S")
+
 def main():
-    print(ts(), "starting up, checking for internet connection...")
+    print(timestamp(), "starting up, checking for internet connection...")
     while True:
         if check_connection('8.8.8.8', 53):
-            print(ts(), "connected to internet")
+            print(timestamp(), "connected to internet")
             break
         time.sleep(1)
-    if sc.rtm_connect(with_team_state=False):
-        print(ts(), "connected to slack")
-        bot_id = sc.api_call('auth.test')['user_id']
-        post_message("Just waking up...")
-        try:
-            nodes = get_nodes()
-            post_message(nodes)
-        except:
-            port_message("Derp! Something went wrong talking to the cluster...")
-        while True:
-            command, channel = parse_bot_commands(sc.rtm_read(), bot_id)
-            if (command and channel == listen_channel):
-                handle_command(command)
-            time.sleep(1)
-    else:
-        print(ts(), 'Connection failed. Exception traceback printed above.')
+    sc.rtm_connect(with_team_state=False)
+    print(timestamp(), "connected to slack")
+    bot_id = sc.api_call('auth.test')['user_id']
+    post_message("Just waking up...")
+    try:
+        nodes = get_nodes()
+        post_message(nodes)
+    except:
+        port_message("Derp! Something went wrong talking to the cluster...")
+    while True:
+        for event in sc.rtm_read():
+            command, ts = parse_event(event, bot_id)
+            if command:
+                handle_command(command, ts)
+        time.sleep(1)
 
-def post_message(message):
-    sc.api_call("chat.postMessage", as_user=True, channel=listen_channel, text=message)
-
-ts = lambda : time.strftime("%Y-%m-%dT%H:%M:%S")
-
-def parse_bot_commands(slack_events, bot_id):
-    for event in slack_events:
-        if event['type'] == 'message' and not 'subtype' in event:
-            user_id, message = parse_direct_mention(event['text'])
-            if user_id == bot_id:
-                event['@timestamp'] = ts()
-                print(event)
-                return message.split(), event['channel']
+def parse_event(event, bot_id):
+    mention = '<@{}>'.format(bot_id)
+    if event['type'] == 'message' and not 'subtype' in event:
+        if mention in event['text'].split() and event['channel'] == listen_channel:
+            event['@timestamp'] = timestamp()
+            print(json.dumps(event))
+            command = event['text'].split()
+            mention_idx = command.index(mention)
+            return command[mention_idx + 1:], event['ts']
     return None, None
 
-def parse_direct_mention(text):
-    regex = '^<@(|[WU].+?)>(.*)'
-    matches = re.search(regex, text)
-    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+def post_reply(message, ts):
+    sc.api_call(
+        "chat.postMessage",
+        as_user=True,
+        channel=listen_channel,
+        text=message,
+        thread_ts=ts
+    )
 
-def handle_command(command):
+def post_message(message):
+    sc.api_call(
+        "chat.postMessage",
+        as_user=True,
+        channel=listen_channel,
+        text=message,
+    )
+
+def random_meme():
+    memes = os.environ['MEMES'].split()
+    return memes[randint(0, len(memes) - 1)]
+
+def handle_command(args, ts):
     response = None
-    if command[0] == 'random':
-        post_message("we're gonna party :fiesta_parrot:")
-    elif command[0] == 'where':
+    command = args[0]
+    args.pop(0)
+    if command == 'show':
+        post_message(random_meme())
+    elif command == 'where':
         post_message("looks like I'm running at {}".format(node_ip))
-    elif command[0] == 'get':
-        if command[1] == 'pods':
+    elif command == 'get':
+        if args[0] == 'pods':
             response = get_pods('default')
-        elif command[1] == 'nodes':
+        elif args[0] == 'nodes':
             response = get_nodes()
-        elif command[1] == 'deployments':
+        elif args[0] == 'deployments':
             response = get_deployments('default')
         else:
             response = ["didn't get that. Try again..."]
-        post_message('returning {} {}...'.format(len(response), command[1]))
-        for l in response:
-            post_message(l)
-    elif command[0] == 'deploy':
-        usage = "deploy containername repository:tag"
+        post_message('{} {}:'.format(len(response), args[0])
+                     + response)
+    elif command == 'deploy':
         post_message("deploying {} to container {} in deployment {}".format(
-            command[2], command[1], command[1])
+            args[1], args[0], args[0])
         )
-        deploy_image(command[1], command[1], command[2])
+        deploy_image(args[0], args[0], args[1])
         handle_command(['get', 'deployments'])
-    elif command[0] == 'scale':
-        usage = "scale deploymentname number"
-        post_message("scaling {} to {} replicas...".format(command[1], command[2]))
-        scale_deployment(command[1], command[2])
+    elif command == 'scale':
+        post_message("scaling {} to {} replicas...".format(args[0], args[1]))
+        scale_deployment(args[0], args[1])
         handle_command(['get', 'pods'])
+    elif 'help' in command:
+        post_message("Here's what you can ask me:\n" + usage)
     else:
-        post_message("Ask again. My responses are limited...")
+        post_message("Didn't catch that. Here's what you can ask:\n" + usage)
 
 main()
