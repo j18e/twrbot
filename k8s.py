df42012bab84fdfe8c5224f801444354436f7c98
James MacFarlane <james@macfarlane.com>
2018-03-10 23:26:15 +0100

    add file upload function

--- k8s.py
+++ k8s.py
@@ -1,19 +1,19 @@
 from kubernetes import client, config
-from subprocess import Popen
 
 config.load_kube_config()
-v1 = client.CoreV1Api()
+core = client.CoreV1Api()
+apps = client.AppsV1Api()
 
 def get_pods(namespace):
     results = ''
-    pod_list = v1.list_namespaced_pod(namespace)
-    for p in v1.list_namespaced_pod(namespace).items:
+    pod_list = core.list_namespaced_pod(namespace)
+    for p in core.list_namespaced_pod(namespace).items:
         results+='{}: {}\n'.format(p.metadata.name, p.status.phase)
     return results
 
 def get_nodes():
     results = ''
-    for n in v1.list_node().items:
+    for n in core.list_node().items:
         a = [i.address for i in n.status.addresses if i.type == 'Hostname'][0]
         s = [i.status for i in n.status.conditions if i.type == 'Ready'][0]
         if s == 'True':
@@ -24,11 +24,11 @@ def get_nodes():
     return results
 
 def get_deployments(namespace):
-    api = client.AppsV1Api()
-    deployments = api.list_namespaced_deployment(namespace).items
+    deployments = apps.list_namespaced_deployment(namespace).items
     results = ''
     for d in deployments:
         if d.status.ready_replicas == None:
+            d.status.ready_replicas = 0
             ready_replicas = '0'
         else:
             ready_replicas = d.status.ready_replicas
@@ -38,25 +38,20 @@ def get_deployments(namespace):
     return results
 
 def scale_deployment(name, namespace, replicas):
-    api = client.AppsV1Api()
-    deployments = api.list_namespaced_deployment(namespace).items
-    deployment = [d for d in deployments if d.metadata.name == name]
-    if len(deployment) == 0:
+    try:
+        deployment = apps.read_namespaced_deployment_scale(name, namespace)
         deployment.spec.replicas = replicas
-        resp = api.patch_namespaced_deployment_scale(name, namespace, deployment)
+        resp = apps.patch_namespaced_deployment_scale(name, namespace, deployment)
         return 'success'
-    else:
-        return "couldn't find a deployment called " + name
+    except:
+        return 'something went wrong. Did you specify an existing deployment?'
 
 def deploy_image(name, namespace, image):
-    api = client.AppsV1Api()
-    deployments = api.list_namespaced_deployment(namespace).items
-    deployment = [d for d in deployments if d.metadata.name == name]
-    if len(deployment) == 0:
-        deployment = deployment[0]
+    try:
+        deployment = apps.read_namespaced_deployment(name, namespace)
         deployment.spec.template.spec.containers[0].image = image
-        resp = api.patch_namespaced_deployment(name, namespace, deployment)
+        resp = apps.patch_namespaced_deployment(name, namespace, deployment)
         return 'success'
-    else:
-        return "couldn't find a deployment called " + name
+    except:
+        return 'something went wrong. Did you specify an existing deployment?'
 
--- listener.py
+++ listener.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 
-import time
 import json
+import time
+import yaml
+import requests
 from os import environ
-from random import randint, shuffle
+from random import shuffle
 from slackclient import SlackClient
 from network import check_connection, get_ip
 from k8s import *
-
-sc = SlackClient(environ['SLACK_TOKEN'])
-listen_channel = environ['SLACK_CHANNEL']
-meme_list = environ['MEMES'].split()
-shuffle(meme_list)
 
 usage = """
 show - wisdom for your journey
@@ -21,11 +18,14 @@ get deployments - list of deployments on k8s cluster
 get nodes - list of nodes in k8s cluster
 deploy dhuser/webapp:12.2 my-webapp - install a new Docker image to the first container of an existing deployment
 scale my-webapp
+
+Upload a YAML and mention me in the comment, I'll try to install it in the cluster!
 """
 
 timestamp = lambda : time.strftime("%Y-%m-%dT%H:%M:%S")
 
-def main():
+def main(sc, ch, meme_list=['empty']):
+    shuffle(meme_list)
     print(timestamp(), "starting up, checking for internet connection...")
     while True:
         if check_connection('8.8.8.8', 53):
@@ -35,72 +35,74 @@ def main():
     sc.rtm_connect(with_team_state=False)
     print(timestamp(), "connected to slack")
     bot_id = sc.api_call('auth.test')['user_id']
-    post_message("Just waking up...")
+    post_message(sc, ch, "Just waking up...")
     try:
         nodes = get_nodes()
-        post_message(nodes)
+        post_message(sc, ch, nodes)
     except:
-        post_message("Derp! Something went wrong talking to the cluster...")
+        post_message(sc, ch, "Derp! Something went wrong talking to the cluster...")
     while True:
         for event in sc.rtm_read():
-            command, ts = parse_event(event, bot_id)
-            if command:
-                handle_command(command, ts)
+            event_type, content = parse_event(ch, event, bot_id)
+            if event_type == 'message':
+                handle_command(sc, ch, content)
+            elif event_type == 'file':
+                handle_file(sc, ch, content)
         time.sleep(1)
 
-def parse_event(event, bot_id):
+def parse_event(ch, event, bot_id):
     mention = '<@{}>'.format(bot_id)
-    if event['type'] == 'message' and not 'subtype' in event:
-        if mention in event['text'].split() and event['channel'] == listen_channel:
-            event['@timestamp'] = timestamp()
-            print(json.dumps(event))
-            command = event['text'].split()
-            mention_idx = command.index(mention)
-            return command[mention_idx + 1:], event['ts']
+    if event['type'] == 'message':
+        if not 'subtype' in event:
+            if mention in event['text'].split() and event['channel'] == ch:
+                event['@timestamp'] = timestamp()
+                print(json.dumps(event))
+                command = event['text'].split()
+                mention_idx = command.index(mention)
+                return 'message', command[mention_idx + 1:]
+        elif event['subtype'] == 'file_share':
+            if mention in event['file']['initial_comment']['comment']:
+                event['@timestamp'] = timestamp()
+                print(json.dumps(event))
+                return 'file', event['file']['url_private']
     return None, None
 
-def post_reply(message, ts):
-    sc.api_call(
-        "chat.postMessage",
-        as_user=True,
-        channel=listen_channel,
-        text=message,
-        thread_ts=ts
-    )
+def post_reply(sc, ch, message, ts):
+    sc.api_call("chat.postMessage", as_user=True, channel=ch,
+                text=message, thread_ts=ts)
 
-def post_message(message):
-    sc.api_call(
-        "chat.postMessage",
-        as_user=True,
-        channel=listen_channel,
-        text=message,
-    )
+def post_message(sc, ch, message):
+    sc.api_call("chat.postMessage", as_user=True, channel=ch,
+                text=message,)
 
-def post_image(url):
-    attachment = {
-        'fallback': 'meme',
-        'image_url': url
-    }
-    sc.api_call(
-        "chat.postMessage",
-        as_user=True,
-        channel=listen_channel,
-        attachments=[attachment]
-    )
+def post_image(sc, ch, url):
+    attachment = {'fallback': 'meme', 'image_url': url}
+    sc.api_call("chat.postMessage", as_user=True, channel=ch,
+                attachments=[attachment])
 
-def handle_command(args, ts):
+def sort_manifests(doc_list):
+    manifests = {'service': [], 'deployment': []}
+    response = ''
+    for d in doc_list:
+        manifests[d['kind']].append(d)
+    for kind, manifest_list in manifests.items():
+        if len(manifest_list) > 0:
+            response+='{} {}s\n'.format(len(manifest_list), kind)
+    return manifests
+
+def handle_command(sc, ch, args):
     response = None
     command = args[0]
     args.pop(0)
     if command == 'show':
-        post_image(meme_list[0])
+        post_image(sc, ch, meme_list[0])
         meme_list.append(meme_list.pop(0))
     elif command == 'where':
         if 'K8S_NODE_IP' in environ:
             location = environ['K8S_NODE_IP']
         else:
             location = get_ip()
-        post_message("looks like I'm running at {}".format(location))
+        post_message(sc, ch, "looks like I'm running at {}".format(location))
     elif command == 'get':
         if args[0] == 'pods':
             response = get_pods('default')
@@ -109,18 +111,34 @@ def handle_command(args, ts):
         elif args[0] == 'deployments':
             response = get_deployments('default')
         else:
-            response = "didn't get that. Try again..."
-        post_message(response)
+            response = "Didn't catch that. Here's what you can ask:\n" + usage
+        post_message(sc, ch, response)
     elif command == 'deploy':
-        post_message("installing image {} to deployment {}".format(
+        post_message(sc, ch, "installing image {} to deployment {}".format(
             args[0], args[1]))
-        post_message(update_image(args[1], 'default', args[0]))
+        post_message(sc, ch, update_image(args[1], 'default', args[0]))
     elif command == 'scale':
-        post_message("scaling {} to {} replicas...".format(args[0], args[1]))
-        post_message(scale_deployment(args[0], 'default', args[1]))
+        post_message(sc, ch, "scaling {} to {} replicas...".format(args[0], args[1]))
+        post_message(sc, ch, scale_deployment(args[0], 'default', args[1]))
     elif 'help' in command:
-        post_message("Here's what you can ask me:\n" + usage)
+        post_message(sc, ch, "Here's what you can ask me:\n" + usage)
     else:
-        post_message("Didn't catch that. Here's what you can ask:\n" + usage)
+        post_message(sc, ch, "Didn't catch that. Here's what you can ask:\n" + usage)
 
-main()
+def handle_file(sc, ch, url):
+    auth = {'Authorization': 'Bearer {}'.format(sc.token)}
+    resp = requests.get(url, headers=auth)
+    try:
+        doc_list = list(yaml.load_all(resp.text))
+        manifests = sort_manifests(doc_list)
+    except:
+        response = "that didn't work. You need to send me only yaml formated \
+Kubernetes manifests of kind deployment or service"
+        return
+    counts = {k: len(l) for k, l in manifests.items() if len(l) > 0}
+    response = yaml.dump(counts, default_flow_style=False)
+    response = 'counts of manifests you want to deploy:\n' + response
+    post_message(sc, ch, response)
+
+main(SlackClient(environ['SLACK_TOKEN']), environ['SLACK_CHANNEL'],
+     meme_list=environ['MEMES'].split())
