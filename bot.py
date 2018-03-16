94da7934a628422aab982cfdd997410e17d01854
James MacFarlane <james@macfarlane.com>
2018-03-16 17:26:00 +0100

    rearrange files, improve stability

---
+++ bot.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+import json
+import time
+from sys import argv
+from os import environ
+from slackclient import SlackClient
+from interactions import get_usage, handle_command
+
+def single_cmd(args):
+    resp, must_confirm = handle_command(args)
+    if must_confirm:
+        print(resp)
+        if input("enter 'confirm' if sure: ") == 'confirm':
+            resp, _ = handle_command(args, confirmed=True)
+        else:
+            resp = "doing nothing..."
+    print(resp)
+
+def cli_loop():
+    print(get_usage(), '\n--------')
+    while True:
+        args = input('enter command: ').split()
+        print()
+        resp, must_confirm = handle_command(args)
+        if must_confirm:
+            if input("enter 'confirm' if sure: ") == 'confirm':
+                resp, _ = handle_command(args, confirmed=True)
+            else:
+                resp = "doing nothing..."
+        print(resp, '\n--------')
+
+def slack_loop():
+    channel = environ['SLACK_CHANNEL']
+    sc = SlackClient(environ['SLACK_TOKEN'])
+    bot_id = slack_startup(sc, channel)
+    while True:
+        args, user = await_command(sc, channel, bot_id)
+        resp, must_confirm = handle_command(args)
+        if must_confirm:
+            post_message(sc, channel, resp)
+            if await_confirmation(sc, channel, bot_id, user, 15):
+                resp, _ = handle_command(args, confirmed=True)
+            else:
+                resp = "timed out, doing nothing..."
+        post_message(sc, channel, resp)
+
+def slack_startup(sc, channel):
+    sc.rtm_connect(with_team_state=False)
+    log_event('connected to slack')
+    bot_id = sc.api_call('auth.test')['user_id']
+    post_message(sc, channel, "just waking up...")
+    return bot_id
+
+def post_message(sc, ch, msg, ts=None):
+    if type(msg) is list:
+        if len(msg) < 20:
+            msg = '\n'.join(msg)
+        elif len(msg) < 200:
+            msg = ' '.join(msg)
+        else:
+            msg = 'too many results to post here...'
+    elif type(msg) is int:
+        msg = str(msg)
+    if msg.startswith('http') and msg.endswith('.jpg'):
+        sc.api_call("chat.postMessage", as_user=True, channel=ch,
+                    attachments=[{'fallback': 'image', 'image_url': msg}])
+    elif ts:
+        sc.api_call("chat.postMessage", as_user=True, channel=ch,
+                    text=msg, thread_ts=ts)
+    else:
+        sc.api_call("chat.postMessage", as_user=True, channel=ch, text=msg,)
+    log_event({'type': 'post', 'message': msg})
+
+def await_command(sc, channel, bot_id):
+    while True:
+        for event in sc.rtm_read():
+            args, user = parse_event(event, channel, bot_id)
+            if args:
+                return args, user
+        time.sleep(1)
+    return None, None
+
+def await_confirmation(sc, channel, bot_id, user, timeout):
+    post_message(sc, channel, "enter 'confirm' to confirm:")
+    while timeout > 0:
+        for event in sc.rtm_read():
+            if (event['type'] == 'message' and not 'subtype' in event and
+                event['channel'] == channel and event['text'] == 'confirm' and
+                event['user'] == user):
+                log_event(event)
+                return True
+        time.sleep(1)
+    return False
+
+def log_event(event):
+    if type(event) is str:
+        result = {'message': event, 'type': 'informational'}
+    elif type(event) is dict:
+        result = event
+    else:
+        print(event)
+        return
+    result['@timestamp'] = time.strftime("%Y-%m-%dT%H:%M:%S")
+    print(json.dumps(result))
+
+def parse_event(event, channel, bot_id):
+    mention = '<@{}>'.format(bot_id)
+    if (event['type'] == 'message' and not 'subtype' in event and
+        event['channel'] == channel and mention in event['text']):
+            log_event(event)
+            args = event['text'].split()
+            args = args[args.index(mention) + 1:]
+            return args, event['user']
+    return None, None
+
+if __name__ == '__main__':
+    if len(argv) < 2:
+        print('must specify slack, cli or cmd as argument')
+        exit()
+    elif argv[1] == 'slack':
+        slack_loop()
+    elif argv[1] == 'cli':
+        cli_loop()
+    elif argv[1] == 'cmd':
+        single_cmd(argv[2:])
+
---
+++ interactions.py
@@ -0,0 +1,94 @@
+import socket
+from os import environ
+from random import shuffle
+from k8s_funcs import get_resources, delete_resource, scale_deployment, deploy_image
+
+def get_usage():
+    return """
+show - wisdom for your journey
+where - ip address of the host I'm running on
+get pods - list of pods on k8s cluster
+get deployments - list of deployments on k8s cluster
+get nodes - list of nodes in k8s cluster
+deploy dhuser/webapp:12.2 my-webapp - install a new Docker image to the first container of an existing deployment
+scale my-webapp
+    """.rstrip()
+
+def handle_command(args, confirmed=False):
+    result = None
+    must_confirm = False
+    command = args[0]
+    args = args[1:]
+    if command == 'show':
+        result = 'no wisdom to speak of...'
+        if 'MEMES' in environ:
+            result = environ['MEMES'].split()
+            shuffle(result)
+            result = result[0]
+    elif command == 'where':
+        result = "looks like I'm running at {}".format(get_ip())
+    elif command == 'get':
+        try:
+            result = get_resources(args[0])
+        except:
+            result = None
+    elif command == 'delete':
+        if confirmed:
+            result = delete_resource(args[0], args[1])
+        else:
+            result = "you're asking to delete {} {}".format(args[0], args[1])
+            must_confirm = True
+    elif command == 'deploy':
+        try:
+            result = 'attempting to install image {} to deployment {}'.format(
+                args[0], args[1])
+            result += update_image(args[1], args[0])
+        except:
+            result = None
+    elif command == 'scale':
+        try:
+            result = 'attempting to scale {} to {} replicas\n'.format(
+                args[0], args[1])
+            result += scale_deployment(args[0], args[1])
+        except:
+            result = None
+    elif 'help' in command:
+        result = "Here's what you can ask me:\n" + get_usage()
+    # endif
+    if result:
+        return result, must_confirm
+    else:
+        return "Didn't catch that. You can say 'help'.", must_confirm
+
+# not currently in use
+def handle_file(url, confirmed=False):
+    auth = {'Authorization': 'Bearer {}'.format(sc.token)}
+    resp = requests.get(url, headers=auth)
+    try:
+        doc_list = list(yaml.load_all(resp.text))
+        manifests = sort_manifests(doc_list)
+    except:
+        return "that didn't work. You need to send me only yaml formated \
+Kubernetes manifests of kind deployment or service"
+    counts = {k: len(l) for k, l in manifests.items() if len(l) > 0}
+    return 'apply manifests:\n' + yaml.dump(counts, default_flow_style=False)
+
+def sort_manifests(doc_list):
+    manifests = {'service': [], 'deployment': []}
+    response = ''
+    for d in doc_list:
+        manifests[d['kind']].append(d)
+    for kind, manifest_list in manifests.items():
+        if len(manifest_list) > 0:
+            response+='{} {}s\n'.format(len(manifest_list), kind)
+    return manifests
+
+def get_ip():
+    if 'K8S_NODE_IP' in environ:
+        result = environ['K8S_NODE_IP']
+    else:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        result = s.getsockname()[0]
+    return result
+
